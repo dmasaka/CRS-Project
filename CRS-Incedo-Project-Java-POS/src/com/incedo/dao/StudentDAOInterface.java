18b4c1d5c823d5aebfd9826578d9db154035e22b
David Masaka <david.masaka@incedoinc.com>
2023-04-14 11:11:16 -0400

    Revert "added testing"
    This reverts commit 250dc8f3ee265f5d2b84d74f48671e13daca2516.

---
+++ CRS-Incedo-Project-Java-POS/src/com/incedo/dao/StudentDAOInterface.java
@@ -0,0 +1,38 @@
+package com.incedo.dao;
+
+import java.util.List;
+
+import com.incedo.bean.Student;
+import com.incedo.exception.ApprovalNotDone;
+import com.incedo.exception.UserNotFoundException;
+
+public interface StudentDAOInterface {
+	/**
+	 * lists all students
+	 * @return
+	 */
+	public List<Student> listOfStudents();
+	/**
+	 * creates new student
+	 * @param stud
+	 */
+	public void add(Student stud);
+	/**
+	 * login for students
+	 * @param username
+	 * @param password
+	 * @return
+	 * @throws UserNotFoundException
+	 * @throws ApprovalNotDone
+	 */
+	public Student login(String username, String password) throws UserNotFoundException, ApprovalNotDone;
+	/**
+	 * lists unproved students
+	 * @return
+	 */
+	public List<Student> listOfUnapprovedStudents();
+	/**
+	 * approves all unproved students
+	 */
+	public void approveAll();
+}
